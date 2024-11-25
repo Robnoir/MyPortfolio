c2ff92c298771d86f7da29ca79e7658b5ad74f2f
Robert Svart <robnoir@Roberts-MacBook-Air.local>
2024-11-25 05:12:37 +0100

    Move all files to development branch

---
+++ MyPortfolio.Application/Dtos/ProjectDto.cs
@@ -0,0 +1,12 @@
+namespace MyPortfolio.Application.Dtos 
+{
+
+    public class ProjectDtos
+    {
+        public string Title {get;set;} = string.Empty;
+        public string Description {get;set;} = string.Empty;
+        public string Link {get;set;} = string.Empty;
+        
+    }
+
+}
